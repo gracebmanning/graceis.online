6cd1d8f1e299e0254d680526182e539fff077f33
Grace Manning <gracebmanning@gmail.com>
2024-09-18 22:56:44 -0700

    fish and ladybug animations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import fish from './assets/fish.png';
+import ladybug from './assets/ladybug.png';
 
 function App() {
   return (
     <div className="App">
       Test App
+      <img className="fish" src={fish} alt="fish with white, blue, a yellow scales." />
+      <img className="ladybug" src={ladybug} />
     </div>
   );
 }
