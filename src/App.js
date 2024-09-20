1979e7da208ebb3f1fc20d58ff4fec37aa7da54b
Grace Manning <gracebmanning@gmail.com>
2024-09-19 20:14:14 -0700

    background opacity

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { daisies } from './assets/assets';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      <img className='backgroundImg' src={daisies} alt='two daisies with smaller purple blooms and surrounding greenery' />
       <Routes>
         <Route path="/" element={<Home/>} />
       </Routes>
