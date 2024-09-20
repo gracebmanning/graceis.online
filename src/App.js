070221bc324879950e71d43e1c73b512ad169153
Grace Manning <gracebmanning@gmail.com>
2024-09-19 20:46:19 -0700

    add 404 page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { daisies } from './assets/assets';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import PageNotFound from './components/404/404';
 
 function App() {
   return (
     <div className="App">
-      <img className='backgroundImg' src={daisies} alt='two daisies with smaller purple blooms and surrounding greenery' />
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
+        <Route path="*" element={<PageNotFound/>} />
       </Routes>
+      <img className='backgroundImg' src={daisies} alt='two daisies with smaller purple blooms and surrounding greenery' />
     </div>
   );
 }
