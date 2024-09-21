9b51414ddd9f44e11df02420a6862bba126b0240
Grace Manning <gracebmanning@gmail.com>
2024-09-21 16:40:59 -0700

    tilt navContainer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,33 +7,33 @@ export default function Home2(){
             <img className='backgroundImg topLeft' id='homeDaisies' src={daisies} alt='two daisies with smaller purple blooms and surrounding greenery' />
             
             <div className='container homeContainer'>
-                <div className='headerText2'>
+                <div className='headerText'>
                     <h1 id='title'>grace manning</h1>
                     <h2 id='tagline'>creative technologist & internet dweller</h2>
                 </div>
                 <div className='navContainer'>
                     <div className='navRow'>
-                        <a className='navButton2' id='aboutButton' href='/about'>
+                        <a className='navButton' id='aboutButton' href='/about'>
                             <img className='navImage' id='fishImage' src={fish} alt='silver fish with blue and yellow accents' />
                             <p className='navText'>about</p>
                         </a>
-                        <a className='navButton2' id='projectsButton' href='/projects'>
+                        <a className='navButton' id='projectsButton' href='/projects'>
                             <img className='navImage' id='ladybugImage' src={ladybug} alt='' />
                             <p className='navText'>projects</p>
                         </a>
                     </div>
                     <div className='navRow'>
-                        <a className='navButton2' id='exploreButton' href='/explore'>
+                        <a className='navButton' id='exploreButton' href='/explore'>
                             <img className='navImage' src={embroideredstar} alt='' />
                             <p className='navText'>explore</p>
                         </a>
                     </div>
                     <div className='navRow'>
-                        <a className='navButton2' id='youtubeButton' href='/'>
+                        <a className='navButton' id='youtubeButton' href='/'>
                             <img className='navImage' src={strawberry} alt='' />
                             <p className='navText'>youtube</p>
                         </a>
-                        <a className='navButton2' id='instagramButton' href='https://www.instagram.com/graceis0nline/' target='_blank' rel='noreferrer'>
+                        <a className='navButton' id='instagramButton' href='https://www.instagram.com/graceis0nline/' target='_blank' rel='noreferrer'>
                             <img className='navImage' id='lunamothImage' src={lunamoth} alt='' />
                             <p className='navText'>instagram</p>
                         </a>
