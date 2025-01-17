d6ff5442e4358126ecb878b63b5cc84e365fe0f5
Grace Manning <gracebmanning@gmail.com>
2025-01-16 19:49:43 -0800

    change surprise page text

--- src/components/Surprise/Surprise.js
+++ src/components/Surprise/Surprise.js
@@ -6,7 +6,7 @@ export default function Surprise(){
         <div style={{width: '100vw', height: '100vh', overflow: 'hidden'}}>
             <img className='backgroundImg topLeft' id='homeDaisies' src={daisies} alt='two daisies with smaller purple blooms and surrounding greenery' />
             <div className='container surpriseContainer'>
-                <h1>surprise!</h1>
+                <h1>surprise! you found an easter egg :-)</h1>
             </div>
         </div>
     )
