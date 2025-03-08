8557248b82196f0868dac6f2fa15ed7e8a6cd43d
Grace Manning <gracebmanning@gmail.com>
2025-03-08 15:34:54 -0800

    remove fiber arts project images

--- src/assets/projects/projects.js
+++ src/assets/projects/projects.js
@@ -6,11 +6,6 @@ import gracesPortal from './thumbnails/graces-portal.png';
 import itneedsmorecolor from './thumbnails/itneedsmorecolor.png';
 import graceIsOnline from './thumbnails/graceis-online.png';
 
-// FIBER ARTS
-import crochetScarf1 from './fiber/1-crochet-scarf1.png';
-import tusnoBag from './fiber/2-tsuno-bag.png';
-import pufferSnoopy from './fiber/3-puffer-snoopy.jpg';
-
 export{
     // THUMBNAILS
     gracemanningCom,
@@ -19,9 +14,4 @@ export{
     gracesPortal,
     itneedsmorecolor,
     graceIsOnline,
-
-    // FIBER ARTS
-    crochetScarf1,
-    tusnoBag,
-    pufferSnoopy,
 }
