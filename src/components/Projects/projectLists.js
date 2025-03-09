8d9dcbfa4a36f23fde67eab1088d7c90d02c56a4
Grace Manning <gracebmanning@gmail.com>
2025-03-08 16:03:17 -0800

    project info to separate file

---
+++ src/components/Projects/projectLists.js
@@ -0,0 +1,58 @@
+import { 
+    // THUMBNAILS
+    gracemanningCom, emmygardnerOnline, classifiedsLol, gracesPortal, itneedsmorecolor, graceIsOnline,
+ } from '../../assets/projects/projects';
+
+export const websites = [
+    {
+        title: 'a portfolio site for emmy',
+        link: 'emmygardner.online',
+        href: 'https://emmygardner.online/',
+        thumbnail: emmygardnerOnline
+    },
+    {
+        title: 'a craigslist knockoff',
+        link: 'classifieds.lol',
+        href: 'https://classifieds.lol/',
+        thumbnail: classifiedsLol
+    },
+    {
+        title: 'a (professional) portfolio website for myself',
+        link: 'grace-manning.com',
+        href: 'https://grace-manning.com/',
+        thumbnail: gracemanningCom
+    },
+    {
+        title: 'my personal portal on the internet',
+        link: 'graces-portal.net',
+        href: 'https://graces-portal.net/',
+        thumbnail: gracesPortal
+    },
+    {
+        title: 'the website that was going to be the one you are currently looking at, until I redesigned the entire thing...',
+        link: 'itneedsmorecolor.com',
+        href: 'https://itneedsmorecolor.com/',
+        thumbnail: itneedsmorecolor
+    },
+    {
+        title: 'the website you are viewing right at this very moment',
+        link: `let's see it again!`,
+        href: 'https://graceis.online/',
+        thumbnail: graceIsOnline
+    },
+]
+
+export const musicVideos = [
+    {
+        title: 'black sheep by metric - lyric video',
+        link: '',
+        href: '',
+        embed: <iframe src="https://www.youtube.com/embed/YpCVh5VX8mc?si=-qsG2CKij9gbVPVo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    },
+    {
+        title: 'talk talk by charli xcx - lyric video',
+        link: '',
+        href: '',
+        embed: <iframe src="https://www.youtube.com/embed/QL--AfXtkCo?si=bKkyBOGr1yJQF7Tk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    },
+]
