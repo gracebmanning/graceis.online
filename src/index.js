bfb40b5cfc4f46dd6ed503cf59e9e08e6348d182
Grace Manning <gracebmanning@gmail.com>
2025-05-11 18:50:09 -0700

    remove App.css, App.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
+import PageNotFound from './components/404/404';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Surprise from './components/Surprise/Surprise';
+import Sources from './components/Sources/Sources';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="*" element={<PageNotFound/>} />
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/projects" element={<Projects/>} />
+      <Route path="/surprise" element={<Surprise/>} />
+      <Route path="/sources" element={<Sources/>} />
+    </Routes>
   </BrowserRouter>
 );
