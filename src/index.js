6452834dce4e36bb8f0df437f96ca41c7ec1948b
Grace Manning <gracebmanning@gmail.com>
2025-07-15 21:41:50 -0700

    removed unused route lines

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import './index.css';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from 'react-router-dom';
-import PageNotFound from './pages/404/404';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import JustForFun, {justForFunRoutes} from './pages/JustForFun/JustForFun';
-import { Posts, postRoutes } from './pages/Blog/BlogPosts';
-import { Tags, tagRoutes } from './pages/Blog/BlogTags';
-import Projects, { projectRoutes } from './pages/Projects/Projects';
-import Surprise from './pages/Surprise/Surprise';
-import Sources from './pages/Sources/Sources';
 import { getRouter } from './routers/subdomains';
-import { MainRouter, BlogRouter } from './routers/routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const App = getRouter();
@@ -21,22 +10,3 @@ root.render(
     <App/>
   </BrowserRouter>
 );
-
-/*
-<Routes>
-      <Route path="*" element={<PageNotFound/>} />
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/just-for-fun" element={<JustForFun/>} />
-      {justForFunRoutes}
-      <Route path="/blog" element={<Posts/>} />
-      {postRoutes}
-      <Route path="/blog/posts" element={<Posts/>} />
-      <Route path="blog/tags" element={<Tags/>} />
-      {tagRoutes}
-      <Route path="/projects" element={<Projects/>} />
-      {projectRoutes}
-      <Route path="/surprise" element={<Surprise/>} />
-      <Route path="/sources" element={<Sources/>} />
-    </Routes>
-*/
