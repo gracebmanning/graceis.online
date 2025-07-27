81358b37aaeac9c1b26f1dc27fc4725665a4469e
Grace Manning <gracebmanning@gmail.com>
2025-07-27 16:41:18 -0700

    remove author schema

--- studio/schemaTypes/index.js
+++ studio/schemaTypes/index.js
@@ -1,6 +1,5 @@
 import blockContent from './blockContent'
 import blogTag from './tag'
 import blogPost from './post'
-import blogAuthor from './author'
 
-export const schemaTypes = [blogPost, blogAuthor, blogTag, blockContent]
+export const schemaTypes = [blogPost, blogTag, blockContent]
