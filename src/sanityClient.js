d987af3825043aa09dfbf73d7a1bdbba9a3c6022
Grace Manning <gracebmanning@gmail.com>
2025-07-26 22:33:13 -0700

    configure sanity client

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,8 +1,8 @@
 import {createClient} from '@sanity/client';
 
 export default createClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  dataset: process.env.REACT_APP_SANITY_DATASET,
+  projectId: process.env.REACT_APP_SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.REACT_APP_SANITY_STUDIO_DATASET,
   useCdn: true, // `false` if you want to ensure fresh data
-  apiVersion: process.env.REACT_APP_SANITY_API_VERSION
+  apiVersion: process.env.REACT_APP_SANITY_STUDIO_API_VERSION
 });
