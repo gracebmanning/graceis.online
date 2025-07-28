d04f8c955b7d37d8cab1a419a3c41e47892dd4fa
Grace Manning <gracebmanning@gmail.com>
2025-07-27 19:16:23 -0700

    reconfigure how project pages use BasicLayout

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -4,12 +4,21 @@ import { Route } from 'react-router-dom';
 import { subpageRoute } from '../../utility/slugify';
 import { IndividualProjectPage } from './IndividualProjectPage/IndividualProjectPage';
 import { List } from '../../components/List/List';
+import { BasicLayout } from '../../layouts/BasicLayout';
 
 // ROUTES FOR ALL PROJECTS
 export const projectRoutes = projects.map((project) => <Route path={subpageRoute('projects', project)} element={<IndividualProjectPage project={project} />} key={project.title} />);
 
 export default function Projects(){
+    // let content = null;
+    // if(postData){
+    //   content = <List header={"all blog posts"} items={postData} type={3} />
+    // } else{
+    //   content = "Loading..."
+    // }
+    let content = <List header={"commercial projects"} items={projects} type={1} />
+
     return(
-        <List header={"commercial projects"} items={projects} type={1} />
+        <BasicLayout title={"Projects"} content={content} />
     )
 }
