e365c542bcfb8b15b33815ca0595e254ca5598c2
Grace Manning <gracebmanning@gmail.com>
2025-08-10 21:19:03 -0700

    new & improved slugify method

--- src/utility/slugify.js
+++ src/utility/slugify.js
@@ -1,4 +1,16 @@
-function slugify(str){
+export function slugify(text){
+    return text
+        .toString()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, '-')
+        .replace(/[^\w-]+/g, '')
+        .replace(/--+/g, '-');
+}
+
+function slugify2(str){
     str = str.toLowerCase().trim();
     str = str.replace(/\s+/g, '-'); // Replace spaces with hyphens
     str = str.replace(/[^a-z0-9-]/g, ''); // Remove non-alphanumeric characters except hyphens
@@ -6,13 +18,13 @@ function slugify(str){
 }
 
 export function projectRoute(proj){
-    return "/projects/" + slugify(proj.title);
+    return "/projects/" + slugify2(proj.title);
 }
 
 export function artRoute(art){
-    return "/just-for-fun/" + slugify(art.title);
+    return "/just-for-fun/" + slugify2(art.title);
 }
 
 export function subpageRoute(subpage, item){
-    return "/" + subpage + "/" + slugify(item.title);
+    return "/" + subpage + "/" + slugify2(item.title);
 }
