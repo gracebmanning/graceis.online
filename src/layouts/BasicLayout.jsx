cec19b060629f38bfd05053ef3692497fdb5ecd1
Grace Manning <gracebmanning@gmail.com>
2025-09-25 18:03:08 -0700

    fix Google Analytics page title

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -4,38 +4,34 @@ import { useLocation } from "react-router-dom";
 import ReactGA from "react-ga4";
 
 export function BasicLayout({ title, content, bg = true }) {
-  // GOOGLE ANALYTICS
-  ReactGA.initialize("G-40KJD20ZZC");
-  let location = useLocation();
+    // GOOGLE ANALYTICS
+    ReactGA.initialize("G-40KJD20ZZC");
+    let location = useLocation();
 
-  useEffect(() => {
-    ReactGA.send({
-      hitType: "pageview",
-      page: location.pathname,
-      title: { title },
-    });
-  }, [location, title]);
+    useEffect(() => {
+        ReactGA.send({
+            hitType: "pageview",
+            page: location.pathname,
+            title: title,
+        });
+    }, [location, title]);
 
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-    window.addEventListener("resize", handleResize);
-    handleResize(); // Initial call to set the initial window size
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        window.addEventListener("resize", handleResize);
+        handleResize(); // Initial call to set the initial window size
 
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
-  return (
-    <div
-      className={windowWidth <= 768 ? "pageContainerMobile" : "pageContainer"}
-    >
-      {windowWidth <= 768 ? <NavbarMobile /> : <Navbar />}
-      <div className={bg ? "sideContainer" : "sideContainer noBg"}>
-        {content}
-      </div>
-    </div>
-  );
+    return (
+        <div className={windowWidth <= 768 ? "pageContainerMobile" : "pageContainer"}>
+            {windowWidth <= 768 ? <NavbarMobile /> : <Navbar />}
+            <div className={bg ? "sideContainer" : "sideContainer noBg"}>{content}</div>
+        </div>
+    );
 }
